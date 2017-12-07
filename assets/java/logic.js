3200f836ef9b3162b0f7b49353df180c8a2f636c
arnold <arnoldrz2@gmail.com>
2017-12-07 12:31:28 -0600

    minor update

--- assets/java/logic.js
+++ assets/java/logic.js
@@ -1,11 +1,11 @@
 //Giphy API: Musical Edition
 //Variables
 //==========================================================================
-var authKey = "hFytTfUDJIvPPcB1J1S3TfBxPqVDcJhr"
+var authKey = "&api_key=hFytTfUDJIvPPcB1J1S3TfBxPqVDcJhr&limit=10";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + authKey;
+
+// Musical Artists/Groups Array
+var topics = [""];
 
 
-
-
-
-
-$("")
+//
