d956db80516707317408069eb7565e1d84d4b3e0
arnold <arnoldrz2@gmail.com>
2017-12-06 18:32:32 -0600

    fixed up html a bit and started javascript

---
+++ assets/java/logic.js
@@ -0,0 +1,11 @@
+//Giphy API: Musical Edition
+//Variables
+//==========================================================================
+var authKey = "hFytTfUDJIvPPcB1J1S3TfBxPqVDcJhr"
+
+
+
+
+
+
+$("")
